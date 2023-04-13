8d7e323814234ecb4ff654ba7d26147306fcc95a
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2023-04-13 11:45:31 -0400

    Configuracion del servidor de configuraciones

--- src/main/kotlin/arquitectura/software/currencyconverter/CurrencyConverterApplication.kt
+++ src/main/kotlin/arquitectura/software/currencyconverter/CurrencyConverterApplication.kt
@@ -1,10 +1,22 @@
 package arquitectura.software.currencyconverter
 
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
-class CurrencyConverterApplication
+@Configuration
+@EnableDiscoveryClient
+class CurrencyConverterApplication{
+	@Bean
+	fun KeycloakConfigResolver(): KeycloakConfigResolver? {
+		return KeycloakSpringBootConfigResolver()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<CurrencyConverterApplication>(*args)
